95189b76b1e016e521b26eb6c0f2b9227c845efe
aaronmorris <aaronmorris@users.noreply.github.com>
2022-05-02 18:56:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const github = require('@actions/github');
 const fs = require("fs");
 
 async function readScannerResults() {
-  const token = core.getInput('repo-token');
+  // booleans still come across as strings so convert to an actual boolean
   const failOnError = core.getInput('fail-on-error').toLowerCase() === 'true' ? true : false;
-  core.info(`token: "${token}"`);
-  const octokit = new github.getOctokit(token);
-
+  const token = core.getInput('repo-token');
   const json = JSON.parse(core.getInput('json'));
-  core.info('json: ' + json[0].engine);
+
+  const octokit = new github.getOctokit(token);
 
   for(let engine of json) {
     const engineName = engine.engine.toUpperCase();
@@ -17,7 +16,6 @@ async function readScannerResults() {
     const annotations = [];
 
     for (let violation of engine.violations) {
-      core.info('next violation');
       const annotation = {
         path: fileName,
         start_line: violation.line ? parseInt(violation.line) : 1,
@@ -28,41 +26,9 @@ async function readScannerResults() {
         end_column: violation.endColumn ? parseInt(violation.endColumn) : parseInt(violation.column)
       };
 
-      core.info(`Annotation: ${annotation}`);
-      try {
-        annotations.push(annotation);
-      }
-      catch (firstError) {
-        core.info('first error');
-        // sometimes the line and column numbers cause issues:
-        // annotation.start_line = 1;
-        // annotation.end_line = 1;
-        // annotation.start_column = 1;
-        // annotation.end_column = 1;
-        // annotation.message = `There was an issue with the line details of the annotation so they will be incorrect.\n${annotation.message}`;
-
-        // annotations.push(annotation);
-      }
-      core.info('annotation pushed');
+      annotations.push(annotation);
     }
 
-    core.info('lets assign those annotations');
-    core.info('failOnError: ' + failOnError);
-    if (failOnError) {
-      core.info('its true');
-    }
-    else {
-      core.info('its false');
-    }
-
-    if (!failOnError) {
-      core.info('its false');
-    }
-    else {
-      core.info('its true');
-    }
-
-    core.info('result: ' + (failOnError ? 'failure' : 'neutral'));
     try {
       const check = await octokit.rest.checks.create({
         owner: github.context.repo.owner,
@@ -79,21 +45,12 @@ async function readScannerResults() {
       });
     }
     catch(error){
-      core.info('format error');
+      core.error('Failed to create annotations.  This is usually due to the line and column numbers returned from the report.  The values will be modified and a second attempt will be made.');
       for (const annotation of annotations) {
-        core.info('annotation.start_line: ' + annotation.start_line);
-        // sometimes the line and column numbers cause issues:
-
         annotation.message = `${annotation.message}\n\nThere was an issue with the line details of the annotation so the end line and end column values were modified.\nOriginal Values:\nStart Line: ${annotation.start_line}\nEnd Line: ${annotation.end_line}\nStart Column: ${annotation.start_column}\nEnd Column: ${annotation.end_column}\n`;
 
-        // annotation.start_line = 1;
-        // annotation.end_line = 1;
         annotation.end_line = annotation.start_line;
-        // annotation.start_column = 1;
-        // annotation.end_column = 1;
         annotation.end_column = annotation.start_column;
-
-        core.info('annotation.start_line: ' + annotation.start_line);
       }
 
       try {
@@ -103,7 +60,7 @@ async function readScannerResults() {
           name: `${engineName} Violation`,
           head_sha: github.context.sha,
           status: 'completed',
-          conclusion: failOnError ? 'failure' : 'warning',
+          conclusion: failOnError ? 'failure' : 'neutral',
           output: {
             title: `${engineName} Violation`,
             summary: `Please review the following ${engineName} Violation`,
@@ -123,44 +80,6 @@ async function readScannerResults() {
 (async () => {
   try {
     await readScannerResults();
-    // checkFileExistence('README.md');
-    // checkFileExistence('LICENSE');
-
-    // if (await checkFileStartsWithHeader('README.md')) {
-    //   core.info('File starts with header so no further action is needed.');
-    //   return;
-    // }
-
-    // get token for octokit
-    // const token = core.getInput('repo-token');
-    // const token = core.getInput('repo-token');
-    // core.info(`token: "${token}"`);
-    // const octokit = new github.getOctokit(token);
-
-    // call octokit to create a check with annotation and details
-    // const check = await octokit.rest.checks.create({
-    //   owner: github.context.repo.owner,
-    //   repo: github.context.repo.repo,
-    //   name: 'Readme Validator',
-    //   head_sha: github.context.sha,
-    //   status: 'completed',
-    //   conclusion: 'failure',
-    //   output: {
-    //     title: 'README.md must start with a title',
-    //     summary: 'Please use markdown syntax to create a title',
-    //     annotations: [
-    //       {
-    //         path: 'README.md',
-    //         start_line: 1,
-    //         end_line: 1,
-    //         annotation_level: 'failure',
-    //         message: 'README.md must start with a header',
-    //         start_column: 1,
-    //         end_column: 1
-    //       }
-    //     ]
-    //   }
-    // });
   }
   catch (error) {
     core.setFailed(error.message);
