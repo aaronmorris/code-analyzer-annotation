959db0c4405b73b78857d99370f1d2a5e3d3e627
aaronmorris <aaronmorris@users.noreply.github.com>
2022-05-02 21:34:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -23,10 +23,7 @@ async function createAnnotation(annotations, engineName, failOnError) {
 async function readScannerResults() {
   // booleans still come across as strings so convert to an actual boolean
   const failOnError = core.getInput('fail-on-error').toLowerCase() === 'true' ? true : false;
-  // const token = core.getInput('repo-token');
   const json = JSON.parse(core.getInput('json'));
-
-  // const octokit = new github.getOctokit(token);
 
   for(let engine of json) {
     const engineName = engine.engine.toUpperCase();
@@ -49,35 +46,22 @@ async function readScannerResults() {
     }
 
     try {
-      // const check = await octokit.rest.checks.create({
-      //   owner: github.context.repo.owner,
-      //   repo: github.context.repo.repo,
-      //   name: `${engineName} Violation`,
-      //   head_sha: github.context.sha,
-      //   status: 'completed',
-      //   conclusion: failOnError ? 'failure' : 'neutral',
-      //   output: {
-      //     title: `${engineName} Violation`,
-      //     summary: `Please review the following ${engineName} Violation`,
-      //     annotations: annotations
-      //   }
-      // });
       await createAnnotation(annotations, engineName, failOnError);
     }
     catch(error){
-      core.warning('Failed to create annotations.  This is usually due to the line and column numbers returned from the report.  The values will be modified and a second attempt will be made.');
+      core.warning('Failed to create annotations on first attempt.  This is usually due to the line and column numbers returned from the report.  The values will be modified and a second attempt will be made.');
       for (const annotation of annotations) {
         annotation.message = `${annotation.message}\n\nThere was an issue with the line details of the annotation so the end line and end column values were modified.\nOriginal Values:\nStart Line: ${annotation.start_line}\nEnd Line: ${annotation.end_line}\nStart Column: ${annotation.start_column}\nEnd Column: ${annotation.end_column}\n`;
 
-        // annotation.end_line = annotation.start_line;
-        // annotation.end_column = annotation.start_column;
+        annotation.end_line = annotation.start_line;
+        annotation.end_column = annotation.start_column;
       }
 
       try {
         await createAnnotation(annotations, engineName, failOnError);
       }
       catch (finalError) {
-        core.error(`Failed to created annotation for the ${engineName} Engine:\n${finalError.message}`);
+        core.error(`Failed to created annotation for the ${engineName} Engine:\n${finalError.message}\nReview the artifacts for more information`);
       }
     }
   }
